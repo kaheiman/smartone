b802b2fb9e751b878f6346ef1701e6233bf70eb3
Marcus <kaheiman@yahoo.com.hk>
2017-10-21 22:58:49 +0800

    first commit

---
+++ smartone_project/View/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  smartone_project
+//
+//  Created by Marcus Man on 21/10/2017.
+//  Copyright © 2017 Marcus Man. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+}
